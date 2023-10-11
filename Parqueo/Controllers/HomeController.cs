aeb9e1e858b35f0ecda4e178e97f2f95f5f64e56
Carlos Pereira <carloscoto0103@gmail.com>
2023-10-10 19:43:55 -0600

    Se agrega el about page

--- Parqueo/Controllers/HomeController.cs
+++ Parqueo/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ public class HomeController : Controller
         return View(GlobalVariables.Tiquetes);
     }
 
-    public IActionResult Privacy()
+    public IActionResult About()
     {
         return View();
     }
